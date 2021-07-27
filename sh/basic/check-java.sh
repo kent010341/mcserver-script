170d864355f2cfbcac48c97c12005ac7874fef9a
kent.chen <kent.chen@hwacom.com>
2021-07-27 17:22:56 +0800

    init check-java.sh

--- sh/basic/check-java.sh
+++ sh/basic/check-java.sh
@@ -1,1 +1,13 @@
 #!/bin/sh
+
+# check Java version
+java -version
+
+# if the command above execute failed, download the latest version of JDK
+if [ $? -eq 0 ]; then
+    echo "Java is installed."
+else
+    # install JDK 16 (for 1.17)
+    echo "Java isn't installed. Start installing..."
+    sudo apt-get isntall openjdk-16-jre 
+fi
