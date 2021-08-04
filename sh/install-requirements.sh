eaff2163b436d8b2ab9cfb12244c8c6fc3212f23
kent.chen <kent.chen@hwacom.com>
2021-08-04 15:46:53 +0800

    edit wrong spelling; add install script

--- sh/install-requirements.sh
+++ sh/install-requirements.sh
@@ -11,3 +11,12 @@ else
     echo "Java isn't installed. Start installing..."
     sudo apt-get install openjdk-16-jdk -y
 fi
+
+# check screen installed
+screen -version
+
+# if the command above execute failed, install screen
+if [ ! $? -eq 0 ]; then
+    echo -e "\033[31m[WARNING] screen isn't installed. Start installing... \033[0m"
+    sudo apt-get install screen -y 
+fi
