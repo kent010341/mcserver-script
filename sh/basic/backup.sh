2b0e26122c07d224cab9e1eeb1fb91835ec4e169
kent.chen <kent.chen@hwacom.com>
2021-08-06 16:07:42 +0800

    fix issue #28

--- sh/basic/backup.sh
+++ sh/basic/backup.sh
@@ -1,16 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 # generating the backup folder
 if [ ! -d "./backup" ]; then
-    echo "\033[31m[WARNING] The folder 'backup' doesn't exist. \033[0m"
+    echo -e "\033[31m[WARNING] The folder 'backup' doesn't exist. \033[0m"
     mkdir backup
-    echo "\033[33m[SUCCESS] Folder 'backup' created. \033[0m"
+    echo -e "\033[33m[SUCCESS] Folder 'backup' created. \033[0m"
 fi
 
 if [ -d "./world" ]; then
     cp -r ./world ./backup/world$(date +"%Y%m%d")
-    echo "\033[33m[SUCCESS] Backup successed. \033[0m"
+    echo -e "\033[33m[SUCCESS] Backup successed. \033[0m"
 else
-    echo "\033[31m[ERROR] The folder 'world' doesn't exist. \033[0m"
+    echo -e "\033[31m[ERROR] The folder 'world' doesn't exist. \033[0m"
     exit 1
 fi
